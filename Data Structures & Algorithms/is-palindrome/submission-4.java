8a484d42369b0d28ede42b034469e1dc024d9713
Reno Melon <92853707+RenoMelon@users.noreply.github.com>
2026-04-04 11:56:54 +0200

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right){
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
+
+            if(!Character.isLetterOrDigit(leftChar)){
+                left++;
+                continue;
+            }
+            else if(!Character.isLetterOrDigit(rightChar)){
+                right--;
+                continue;
+            }
+            else{
+                if(Character.toLowerCase(leftChar) == Character.toLowerCase(rightChar)){
+                    left++;
+                    right--;
+                }else{
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+}
+
+
