b93a861d015838ebf7dbf4eb59a0570facf78c0d
Reno Melon <92853707+RenoMelon@users.noreply.github.com>
2026-04-21 09:49:23 +0200

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        StringBuilder res = new StringBuilder();
+
+        for(int i = 0; i < strs[0].length(); i++){
+            char c = strs[0].charAt(i);
+            for(int j = 1; j < strs.length; j++){
+                if(strs[j].length() <= i || strs[j].charAt(i) != c){
+                    return res.toString();
+                }
+            }
+            res.append(c);
+        }
+
+        return res.toString();
+    }
+}
